c6dae01c0cc6cb43c436c5418fa186d9dccf3f6e
smshah121 <smshah.2003@gmail.com>
2025-10-13 01:35:02 +0500

    changes saved

--- src/App.jsx
+++ src/App.jsx
@@ -521,7 +521,7 @@ function App() {
               initial={{ x: -100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
               transition={{ duration: 0.8 }}
-              className="w-full max-w-5xl bg-white/5 backdrop-blur-xl rounded-2xl flex flex-col md:flex-row items-center p-6"
+              className="w-full max-w-5xl bg-white/5 backdrop-blur-xl mb-2 md:mb-0 rounded-2xl flex flex-col md:flex-row items-center p-6"
             >
               {/* Image */}
               <div className="flex-shrink-0 w-full md:w-1/3 flex justify-center">
@@ -588,7 +588,7 @@ function App() {
   </motion.button>
 
   {dropdownOpen === index && (
-    <div className="absolute mt-2 bg-[#1e293b] text-white rounded-lg shadow-lg z-50 w-40">
+    <div className="absolute mt-2 bg-[#1e293b] text-white rounded-lg text-xs shadow-lg z-50 w-40">
       {project.source?.frontend && (
         <a
           href={project.source.frontend}
